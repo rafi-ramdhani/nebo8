d8dd6e15967a20e3bce7cf76ed79dac617a807e3
Rafi Ramdhani Azis <rafiramdhani20@gmail.com>
2023-11-06 01:11:44 +0700

    feat: moving services

--- src/app/ServiceSection/index.tsx
+++ src/app/ServiceSection/index.tsx
@@ -1,62 +1,198 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { PageSection, YoutubeVideo } from "../components";
 import NeboLogo from "../components/NeboLogo";
 import styles from "./styles.module.css";
 import Image from "next/image";
 
+type ServiceClassName = {
+  left: string;
+  right: string;
+  used: string;
+};
+
 type Service =
-  | "6y8d1oUClQE"
-  | "4YneDg6dH4U"
-  | "n62M2pd0-p8"
-  | "UIVhfDWSSKY"
-  | "9vINAPof9Lk"
-  | "U7ajKfnMby0"
-  | "YCMtaTi6kMQ";
+  | {
+      name: "Photograph";
+      videoId: "6y8d1oUClQE";
+      className: ServiceClassName;
+    }
+  | {
+      name: "2D & 3D Animation";
+      videoId: "4YneDg6dH4U";
+      className: ServiceClassName;
+    }
+  | {
+      name: "Commercial";
+      videoId: "n62M2pd0-p8";
+      className: ServiceClassName;
+    }
+  | {
+      name: "Video Production";
+      videoId: "UIVhfDWSSKY";
+      className: ServiceClassName;
+    }
+  | {
+      name: "Corporate Video";
+      videoId: "9vINAPof9Lk";
+      className: ServiceClassName;
+    }
+  | {
+      name: "Documentary Video";
+      videoId: "U7ajKfnMby0";
+      className: ServiceClassName;
+    }
+  | {
+      name: "Lorem Ipsum";
+      videoId: "YCMtaTi6kMQ";
+      className: ServiceClassName;
+    };
+
+const serviceClassName = (
+  order: "One" | "Two" | "Three" | "Four" | "Five" | "Six" | "Seven",
+  direction: "Left" | "Right"
+) => {
+  const uniqueClassName = styles[`serviceText${order}${direction}`];
+  const floatClassName = styles[`serviceText${order}Float`];
+  return `${styles.serviceText} ${uniqueClassName} ${floatClassName}`;
+};
 
 const ServiceSection = () => {
-  const [selectedService, setSelectedService] =
-    useState<Service>("UIVhfDWSSKY");
-  const [services, setServices] = useState<Service[]>([
-    "6y8d1oUClQE",
-    "4YneDg6dH4U",
-    "n62M2pd0-p8",
-    "UIVhfDWSSKY",
-    "9vINAPof9Lk",
-    "U7ajKfnMby0",
-    "YCMtaTi6kMQ",
+  const [selectedServiceIndex, setSelectedServiceIndex] = useState(3);
+
+  const [selectedService, setSelectedService] = useState<Service>({
+    name: "Video Production",
+    videoId: "UIVhfDWSSKY",
+    className: {
+      left: serviceClassName("Four", "Left"),
+      right: serviceClassName("Four", "Right"),
+      used: serviceClassName("Four", "Left"),
+    },
+  });
+
+  const [serviceClassNames, setServiceClassNames] = useState<
+    ServiceClassName[]
+  >([
+    {
+      left: serviceClassName("One", "Left"),
+      right: serviceClassName("One", "Right"),
+      used: serviceClassName("One", "Left"),
+    },
+    {
+      left: serviceClassName("Two", "Left"),
+      right: serviceClassName("Two", "Right"),
+      used: serviceClassName("Two", "Left"),
+    },
+    {
+      left: serviceClassName("Three", "Left"),
+      right: serviceClassName("Three", "Right"),
+      used: serviceClassName("Three", "Left"),
+    },
+    {
+      left: serviceClassName("Four", "Left"),
+      right: serviceClassName("Four", "Right"),
+      used: serviceClassName("Four", "Left"),
+    },
+    {
+      left: serviceClassName("Five", "Left"),
+      right: serviceClassName("Five", "Right"),
+      used: serviceClassName("Five", "Left"),
+    },
+    {
+      left: serviceClassName("Six", "Left"),
+      right: serviceClassName("Six", "Right"),
+      used: serviceClassName("Six", "Left"),
+    },
+    {
+      left: serviceClassName("Seven", "Left"),
+      right: serviceClassName("Seven", "Right"),
+      used: serviceClassName("Seven", "Left"),
+    },
   ]);
 
-  useEffect(() => {
-    const selectTimeout = setTimeout(() => {
-      setSelectedService(services[3]);
-    }, 500);
-
-    return () => clearTimeout(selectTimeout);
-  }, [services]);
+  const services: Service[] = useMemo(() => {
+    return [
+      {
+        name: "Photograph",
+        videoId: "6y8d1oUClQE",
+        className: serviceClassNames[0],
+      },
+      {
+        name: "2D & 3D Animation",
+        videoId: "4YneDg6dH4U",
+        className: serviceClassNames[1],
+      },
+      {
+        name: "Commercial",
+        videoId: "n62M2pd0-p8",
+        className: serviceClassNames[2],
+      },
+      {
+        name: "Video Production",
+        videoId: "UIVhfDWSSKY",
+        className: serviceClassNames[3],
+      },
+      {
+        name: "Corporate Video",
+        videoId: "9vINAPof9Lk",
+        className: serviceClassNames[4],
+      },
+      {
+        name: "Documentary Video",
+        videoId: "U7ajKfnMby0",
+        className: serviceClassNames[5],
+      },
+      {
+        name: "Lorem Ipsum",
+        videoId: "YCMtaTi6kMQ",
+        className: serviceClassNames[6],
+      },
+    ];
+  }, [serviceClassNames]);
 
   const handleSelect = (direction: "left" | "right") => {
     switch (direction) {
       case "left":
-        setServices((previousServices) => {
-          const firstService = previousServices[0];
-          const newServices = [
-            ...previousServices.slice(1, previousServices.length),
-            firstService,
-          ];
-          return newServices;
+        setServiceClassNames((previousServiceClassNames) => {
+          const lastServiceClassName =
+            previousServiceClassNames[previousServiceClassNames.length - 1];
+          const newServiceClassNames = [
+            lastServiceClassName,
+            ...previousServiceClassNames.slice(
+              0,
+              previousServiceClassNames.length - 1
+            ),
+          ].map((serviceClassName) => ({
+            ...serviceClassName,
+            used: serviceClassName.left,
+          }));
+          const selectedServiceIndex = newServiceClassNames.findIndex(
+            ({ used }) => used.includes("serviceTextFour")
+          );
+          setSelectedServiceIndex(selectedServiceIndex);
+          return newServiceClassNames;
         });
         break;
 
       case "right":
-        setServices((previousServices) => {
-          const lastService = previousServices[previousServices.length - 1];
-          const newServices = [
-            lastService,
-            ...previousServices.slice(0, previousServices.length - 1),
-          ];
-          return newServices;
+        setServiceClassNames((previousServiceClassNames) => {
+          const firstServiceClassName = previousServiceClassNames[0];
+          const newServiceClassNames = [
+            ...previousServiceClassNames.slice(
+              1,
+              previousServiceClassNames.length
+            ),
+            firstServiceClassName,
+          ].map((serviceClassName) => ({
+            ...serviceClassName,
+            used: serviceClassName.right,
+          }));
+          const selectedServiceIndex = newServiceClassNames.findIndex(
+            ({ used }) => used.includes("serviceTextFour")
+          );
+          setSelectedServiceIndex(selectedServiceIndex);
+          return newServiceClassNames;
         });
         break;
 
@@ -65,10 +201,18 @@ const ServiceSection = () => {
     }
   };
 
+  useEffect(() => {
+    const selectServiceTimeout = setTimeout(() => {
+      setSelectedService(services[selectedServiceIndex]);
+    }, 400);
+
+    return () => clearTimeout(selectServiceTimeout);
+  }, [selectedServiceIndex, services]);
+
   return (
     <PageSection section="service">
       <div className={styles.serviceContent}>
-        <YoutubeVideo videoId={selectedService} />
+        <YoutubeVideo videoId={selectedService.videoId} />
         <NeboLogo />
         <div className={styles.selection}>
           <Image
@@ -78,49 +222,20 @@ const ServiceSection = () => {
             alt="Left Arrow Icon"
             onClick={() => handleSelect("left")}
           />
+
           <div className={styles.serviceTextContainer}>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-30rem", left: "-40rem" }}
-            >
-              {services[0]}
-            </h3>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-20rem", left: "-50rem" }}
-            >
-              {services[1]}
-            </h3>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-8rem", left: "-28rem" }}
-            >
-              {services[2]}
-            </h3>
-            <h3
-              className={`${styles.serviceText} ${styles.selectedServiceText}`}
-            >
-              {services[3]}
-            </h3>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-8rem", left: "28rem" }}
-            >
-              {services[4]}
-            </h3>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-20rem", left: "50rem" }}
-            >
-              {services[5]}
-            </h3>
-            <h3
-              className={styles.serviceText}
-              style={{ top: "-30rem", left: "40rem" }}
-            >
-              {services[6]}
-            </h3>
+            {services.map((service) => {
+              return (
+                <h3
+                  key={`${service.name}-${service.videoId}`}
+                  className={service.className.used}
+                >
+                  {service.name}
+                </h3>
+              );
+            })}
           </div>
+
           <Image
             width={38}
             height={53}
