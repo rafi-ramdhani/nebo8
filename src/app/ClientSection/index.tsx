9a2346bb28832d6cdac3c13bee2acb799c286daf
Rafi Ramdhani Azis <rafiramdhani20@gmail.com>
2023-11-07 22:00:50 +0700

    feat: clients

--- src/app/ClientSection/index.tsx
+++ src/app/ClientSection/index.tsx
@@ -1,7 +1,60 @@
+import Image from "next/image";
 import { PageSection } from "../components";
+import styles from "./styles.module.css";
 
 const ClientSection = () => {
-  return <PageSection section="client"></PageSection>;
+  const clients = [
+    "bi",
+    "mandiri",
+    "btpn",
+    "toyota",
+    "honda",
+    "puma",
+    "adidas",
+    "nb",
+    "logitech",
+  ];
+
+  const imageSizes = "(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw";
+
+  return (
+    <PageSection section="client">
+      <div className={styles.clientContent}>
+        <h2 className={styles.ourClientText}>our client</h2>
+        <div className={styles.clientsContainer}>
+          {clients.map((client) => {
+            return (
+              <div key={client} className={styles[client]}>
+                <Image
+                  fill
+                  sizes={imageSizes}
+                  src={`/${client}.png`}
+                  alt={client}
+                />
+              </div>
+            );
+          })}
+        </div>
+
+        <div className={styles.astronaut}>
+          <Image
+            fill
+            sizes={imageSizes}
+            src="/astronaut2.png"
+            alt="Astronaut Image"
+          />
+        </div>
+        <div className={styles.spaceship}>
+          <Image
+            fill
+            sizes={imageSizes}
+            src="/spaceship.png"
+            alt="Spaceship Image"
+          />
+        </div>
+      </div>
+    </PageSection>
+  );
 };
 
 export default ClientSection;
--- src/app/ContactSection/index.tsx
+++ src/app/ContactSection/index.tsx
@@ -3,46 +3,38 @@ import { PageSection } from "../components";
 import styles from "./styles.module.css";
 
 const ContactSection = () => {
+  const imageSizes = "(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw";
+
   return (
     <PageSection section="contact">
       <div className={styles.contactContent}>
-        <Image
-          className={styles.when}
-          width={1418}
-          height={483}
-          src="/when.png"
-          alt="When Image"
-        />
+        <div className={styles.when}>
+          <Image fill sizes={imageSizes} src="/when.png" alt="When Image" />
+        </div>
         <div className={styles.whatsappContainer}>
+          <div className={styles.brief}>
+            <Image fill sizes={imageSizes} src="/brief.png" alt="Brief Image" />
+          </div>
+          <div className={styles.whatsapp}>
+            <Image
+              fill
+              sizes={imageSizes}
+              src="/whatsapp.png"
+              alt="Whatsapp Image"
+            />
+          </div>
+        </div>
+        <div className={styles.astronaut}>
           <Image
-            className={styles.brief}
-            width={330}
-            height={265}
-            src="/brief.png"
-            alt="Brief Image"
-          />
-          <Image
-            className={styles.whatsapp}
-            width={148}
-            height={148}
-            src="/whatsapp.png"
-            alt="Whatsapp Image"
+            fill
+            sizes={imageSizes}
+            src="/astronaut.png"
+            alt="Astronaut Image"
           />
         </div>
-        <Image
-          className={styles.astronaut}
-          width={1031}
-          height={846}
-          src="/astronaut.png"
-          alt="Astronaut Image"
-        />
-        <Image
-          className={styles.rocket}
-          width={1131}
-          height={1237}
-          src="/rocket.png"
-          alt="Rocket Image"
-        />
+        <div className={styles.rocket}>
+          <Image fill sizes={imageSizes} src="/rocket.png" alt="Rocket Image" />
+        </div>
       </div>
     </PageSection>
   );
