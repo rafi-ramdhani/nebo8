52b08228217570b3081cb25b7991b374106da9ee
Rafi Ramdhani Azis <rafiramdhani20@gmail.com>
2023-11-09 09:41:34 +0700

    fix: temp remove image in footer section

--- src/app/FooterSection/index.tsx
+++ src/app/FooterSection/index.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import { PageSection } from "../components";
 import styles from "./styles.module.css";
 
 const FooterSection = () => {
   return (
     <PageSection section="footer">
-      <div className={styles.footerContent}>
-        <Image />
-      </div>
+      <div className={styles.footerContent}></div>
     </PageSection>
   );
 };
