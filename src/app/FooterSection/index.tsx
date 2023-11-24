38432d071ad2cf21bca8c6ea758c485dea8d355e
Rafi Ramdhani Azis <rafiramdhani20@gmail.com>
2023-11-24 15:46:57 +0700

    feat: slight improvements

--- src/app/FooterSection/index.tsx
+++ src/app/FooterSection/index.tsx
@@ -7,7 +7,7 @@ const FooterSection = () => {
     <PageSection section="footer">
       <div className={styles.footerContent}>
         <div className={styles.textContainer}>
-          <h1 className={styles.neboText}>pt nebo8 indonesia</h1>
+          <h1 className={styles.neboText}>pt. nebo creative jaya</h1>
           <h4 className={styles.addressText}>
             Jl. BDN II No.22, RT.2/RW.11, Cilandak Barat, Kec. Cilandak, Kota
             Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12430
